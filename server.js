4751347fdb874ac92fb96fa1ed4c4db410d27895
Dikmit02 <dikshamittal21297@gmail.com>
2019-08-06 18:24:10 +0530

    sending only to selected on

--- server.js
+++ server.js
@@ -7,12 +7,21 @@ const app=express();
 const server=http.createServer(app) 
 const io=soketio(server)
 
+let usersockets = {}
+
 app.use('/',express.static(path.join(__dirname,'frontend')))
 
 
 io.on('connection',(socket)=>{
     console.log("New socket formed from"+socket.id)
     socket.emit('connected')
+
+
+    socket.on('login', (data) => {
+        // username is in data.user
+        usersockets[data.user] = socket.id
+        console.log(usersockets)
+    })
 
 
     
@@ -22,7 +31,16 @@ io.on('connection',(socket)=>{
         // if we use socket.broadcast.emit, only others get it
         // io.emit('recv_msg',data)
 
-        socket.broadcast.emit('recv_msg',data)
+        if (data.message.startsWith('@')) {
+            //data.message = "@a: hello"
+            // split at :, then remove @ from beginning
+            let recipient = data.message.split(':')[0].substr(1)
+            let rcptSocket = usersockets[recipient]
+            // sending to only selected one
+            io.to(rcptSocket).emit('recv_msg', data)
+        } else {
+            socket.broadcast.emit('recv_msg', data)            
+        }
         
     })
 })
